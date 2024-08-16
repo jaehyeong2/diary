094a5b5e16c6fbdf20bc64f76bc03ed9fb385d3e
jaehyeong <wogud1514@gmail.com>
2024-08-16 12:05:35 +0900

    feature: diary 조회 api 추가

--- src/main/kotlin/jjfactory/diary/presentation/DiaryApi.kt
+++ src/main/kotlin/jjfactory/diary/presentation/DiaryApi.kt
@@ -1,8 +1,11 @@
 package jjfactory.diary.presentation
 
 import jjfactory.diary.common.response.CommonResponse
+import jjfactory.diary.config.security.UserAuthentication
 import jjfactory.diary.domain.diary.DiaryCommand
+import jjfactory.diary.domain.diary.DiaryInfo
 import jjfactory.diary.domain.diary.DiaryService
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -11,11 +14,19 @@ class DiaryApi(
     private val diaryService: DiaryService
 ) {
 
+    @GetMapping("/{id}")
+    fun getDiary(@PathVariable id: Long): CommonResponse<DiaryInfo.Detail> {
+        val userAuthentication = SecurityContextHolder.getContext().authentication as UserAuthentication
+        return CommonResponse(diaryService.getDiary(id, userAuthentication.getUserId()))
+    }
+
     @PostMapping
     fun writeDiary(@RequestBody command: DiaryCommand.Create): CommonResponse<Long> {
+        val userAuthentication = SecurityContextHolder.getContext().authentication as UserAuthentication
+
         return CommonResponse(
             diaryService.write(
-                userId = 2L,
+                userId = userAuthentication.getUserId(),
                 command = command
             )
         )
@@ -23,8 +34,10 @@ class DiaryApi(
 
     @PatchMapping("/{id}")
     fun modify(@PathVariable id: Long, @RequestBody command: DiaryCommand.Modify): CommonResponse<Unit> {
+        val userAuthentication = SecurityContextHolder.getContext().authentication as UserAuthentication
+
         diaryService.modify(
-            userId = 2L,
+            userId = userAuthentication.getUserId(),
             id = id,
             command = command
         )
